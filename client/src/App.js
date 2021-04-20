2868ef8e8aaf8ec1565e113d7cffd802c1d97ba8
Smith Scarborough <smithscarborough@gmail.com>
2021-04-20 15:28:31 -0500

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Switch from 'react-bootstrap/esm/Switch';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Switch>
+        <NavBar />
+      </Switch>
     </div>
   );
 }
