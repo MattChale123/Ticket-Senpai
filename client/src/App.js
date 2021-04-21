d3ade0b6e74edce3e88f460a66df379b86ca36fd
Brandon <woolstonbj11@gmail.com>
2021-04-21 10:22:01 -0400

    pulled inital data from seatgeaks api

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SeatGeek from './components/SeatGeek';
 
 function App() {
   return (
     <div className="App">
+      <SeatGeek />
       <NavBar />
     </div>
   );
