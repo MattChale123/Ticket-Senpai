c7ed88649bf7ba4c43410c952ac06d2f38ab1844
Matthew Chaleski <chaleski76@gmail.com>
2021-04-29 15:57:37 -0400

    fixed invalid location bug and added a warning

--- client/src/components/GoogleMaps.js
+++ client/src/components/GoogleMaps.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
 import { Loader } from "@googlemaps/js-api-loader"
-import usePosition from '../hooks/usePosition'
 
 const loader = new Loader({
     apiKey: 'AIzaSyBxCFPMNQ1YqokuJM8pwSnWnqNByNzEPl0',
@@ -9,15 +8,14 @@ const loader = new Loader({
   });
 
 export default function GoogleMaps(props) {
-    const  { latitude, longitude } = usePosition()
     const [ results, setResults ] = useState([])
-    console.log(results)
     const [ googleMaps, setGoogleMaps ] = useState(null)
+    const [ error, setError ] = useState('')
     const [ form, setForm ] = useState({
         origin: '',
         destination: [props.address.address, props.address.city, props.address.state, props.address.postal_code]
     })
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
         const position = new googleMaps.LatLng(props.eventLatitude, props.eventLng)
@@ -26,12 +24,17 @@ export default function GoogleMaps(props) {
         console.log(position)
         service.getDistanceMatrix({
             origins: [...form.origin].length > 0 ? [form.origin] : [position],
-            destinations: [props.address.address, props.address.city, props.address.state, props.address.postal_code] || [form.destination],
+            destinations: [`${props.address.address}, ${props.address.city}, ${props.address.state}, ${props.address.postal_code}`] || [form.destination],
             travelMode: 'DRIVING',
         })
         .then(data => {
             console.log(data)
-            setResults(data.rows)
+            if (data.rows[0].elements[0].status === "NOT_FOUND" ) {
+                setError('Invalid location. Please set origin to a valid address')
+            } else {
+                setResults(data.rows)
+            }
+            
         })
     }
     const handleChange = (e) => {
@@ -52,11 +55,15 @@ export default function GoogleMaps(props) {
     const metersToMiles = (num) => {
         return Math.round(num / 1000 * 0.621371)
     }
+    const errorStyling = {
+        color: "red",
+        fontWeight: "bold"
+      }
 
     return (
         <div>
             <Form onSubmit={handleSubmit} style={styling}>
-                <Form.Control type="text" placeholder="Starting location" onChange={handleChange} value={form.origin} name='origin'/>
+                <Form.Control type="text" placeholder="123 Main St, State, GA 12345" onChange={handleChange} value={form.origin} name='origin'/>
                     <br />
                 <Form.Control type="text" placeholder="Destination" onChange={handleChange} value={form.destination} name='destination'/>
                     <br />
@@ -71,7 +78,8 @@ export default function GoogleMaps(props) {
                         </Card.Body>
                     </Card>
                 )
-            })}  
+            })}
+            {(error === 'Invalid location. Please set origin to a valid address') && <p style={errorStyling}>*{error}*</p>}
         </div>
     )
 }
