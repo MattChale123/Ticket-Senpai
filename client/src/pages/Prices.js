33ceb52e67342d683f8ccaa2b1ab504cf7a2cf55
Angel Deluna <angelluna2016@gmail.com>
2021-04-29 15:57:40 -0400

    made prices.js responsive

--- client/src/pages/Prices.js
+++ client/src/pages/Prices.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Button, Col, Container, Row, Table } from 'react-bootstrap'
 import { useLocation } from 'react-router-dom'
 import SeatGeekCard from '../components/SeatGeekCard'
 import StubHubCard from '../components/StubHubCard'
@@ -7,6 +7,8 @@ import TicketMasterCard from '../components/TicketMasterCard'
 import useStubHub from '../hooks/useStubHub'
 import GoogleMaps from '../components/GoogleMaps'
 import usePosition from '../hooks/usePosition'
+import moment from 'moment'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Prices(props) {
     const location = useLocation()
@@ -14,13 +16,25 @@ export default function Prices(props) {
     const [stubHubInfo, setStubHubInfo] = useState([])
     const stubHub = useStubHub()
     const event = location.state.event
-    const  { latitude, longitude } = usePosition()
+    const { latitude, longitude } = usePosition()
     const address = {
         city: event.venue.city,
         state: event.venue.state,
         address: event.venue.address,
         postal_code: event.venue.postal_code
     }
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-device-width: 1224px)'
+    })
+    const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isTabletOrMobileDevice = useMediaQuery({
+        query: '(max-device-width: 1224px)'
+    })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+
+
+
 
     useEffect(() => {
         fetchAll()
@@ -75,32 +89,127 @@ export default function Prices(props) {
         <Container style={{ textAlign: "center" }}>
             <Row >
                 <Col className="mt-3">
-                    <h1> Prices for {event.title} in {event.venue.city} {event.venue.state}</h1>
+                    <h1> Prices for {event.title} in {event.venue.city}, {event.venue.state}</h1>
                 </Col>
             </Row>
-            <Row>
-                <Col sm={4}>
-                    <SeatGeekCard event={event} />
-                </Col>
-                <Col sm={4}>
-                    <StubHubCard event={stubHubInfo} />
-                </Col>
-                <Col sm={4}>
-                    <TicketMasterCard event={TicketMaster} />
-                </Col>
-            </Row>
+            {isDesktopOrLaptop &&
+                <>
+                    <Row>
+                        <Col sm={4}>
+                            <SeatGeekCard event={event} />
+                        </Col>
+                        <Col sm={4}>
+                            <StubHubCard event={stubHubInfo} />
+                        </Col>
+                        <Col sm={4}>
+                            <TicketMasterCard event={TicketMaster} />
+                        </Col>
+                    </Row>
+                </>
+            }
+            {isTabletOrMobileDevice &&
+                <>
+                    <Table  variant="dark">
+                        <thead>
+                            <tr>
+                                <th>{event.title}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> <img src={event.performers[0].image} alt="event"></img> </td>
+                            </tr>
+                            <tr>
+                                Date: {moment.parseZone(event.datetime_local).format('MM/D/YYYY')}
+                                <td> </td>
+                            </tr>
+                            <tr>
+                                <td>Venue: <a href={`https://google.com/maps/search/${event.venue.name_v2} ${event.venue.city} ${event.venue.country}`} target="_blank" rel="noreferrer">
+                                    <span>{event.venue.name_v2}</span>
+                                </a></td>
+                            </tr>
+                            <Table  variant="dark">
+                                <thead>
+                                    <tr>
+                                        <th>Seller</th>
+                                        <th>Low</th>
+                                        <th>Average</th>
+                                        <th>High</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>SeatGeek</td>
+                                        <td>
+                                            <Button className="ml-2 mr-2" href={event.url} variant="success">${event.stats.lowest_price}</Button>
+                                        </td>
+                                        <td>
+                                            <Button className="mr-2" href={event.url} name="averagePrice" variant="primary">${event.stats.average_price}</Button>
+                                        </td>
+                                        <td>
+                                            <Button href={event.url} variant="danger">${event.stats.highest_price}</Button>
+                                        </td>
+
+                                    </tr>
+                                    <tr>
+                                        <td>StubHub</td>
+                                        {!stubHubInfo.id ? (
+                                            <td> No tickets currently available from this vendor.</td>
+                                        ) : (
+                                            <>
+                                                <td>
+                                                    <Button href={`https://www.stubhub.com//${stubHubInfo.webURI}`} variant="success">${Math.round(stubHubInfo.ticketInfo.minListPrice)}</Button>
+                                                </td>
+                                                <td>
+                                                    <Button href={`https://www.stubhub.com//${stubHubInfo.webURI}`} name="averagePrice" variant="primary">${
+                                                        Math.round((stubHubInfo.ticketInfo.maxListPrice + stubHubInfo.ticketInfo.minListPrice) / 2)}
+                                                    </Button>
+                                                </td>
+                                                <td>
+                                                    <Button href={`https://www.stubhub.com//${stubHubInfo.webURI}`} variant="danger">
+                                                        ${Math.round(stubHubInfo.ticketInfo.maxListPrice)}
+                                                    </Button>
+                                                </td>
+                                            </>
+                                        )}
+                                    </tr>
+                                    <tr>
+                                        <td>ticketmaster</td>
+                                        {!TicketMaster.id ? (
+                                            <td> No tickets currently available from this vendor.</td>
+                                        ) : (
+                                            <>
+                                                <td>
+                                                    <Button href={TicketMaster.url} variant="success">${Math.round(TicketMaster.priceRanges[0].min)}</Button>
+                                                </td>
+                                                <td>
+                                                    <Button href={TicketMaster.url} name="averagePrice" variant="primary">
+                                                        ${Math.round((TicketMaster.priceRanges[0].min + TicketMaster.priceRanges[0].max) / 2)}
+                                                    </Button>
+                                                </td>
+                                                <td>
+                                                    <Button href={TicketMaster.url} variant="danger">${Math.round(TicketMaster.priceRanges[0].max)}</Button>
+                                                </td>
+                                            </>
+                                        )}
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </tbody>
+                    </Table>
+                </>
+            }
+
             <Row style={styling}>
                 <Col className="mt-3">
                     <h3>Distance to event.</h3>
                     <h5>Either use current location, or enter origin.</h5>
-                    <p>(Location manuelly filled if accepted)</p>
+                    <p>(Location manually filled if accepted)</p>
                     <div>
                         <GoogleMaps eventLatitude={latitude} eventLng={longitude} address={address} />
                     </div>
                 </Col>
             </Row>
-
-
         </Container>
 
     )
